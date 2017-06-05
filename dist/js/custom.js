3907fdce1fbfbad20a8ada1affad57d36004f65e
Chandrakant Shelar <chandrakanth.shelar@konsult.folksam.se>
2017-06-05 02:21:18 +0200

    deleted unwanted lines

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -16,16 +16,6 @@
 			ticks_snap_bounds: 0
 		});
 
-/* 		$( ".slider-example" ).each(function( index ) {
-
-			var slider=("#slider"+(index+1));
-			var input=$(this).find(".slider-input");
-			$(this).find(".slider-handle").click(function() {
-
-				input.val($(slider).slider('getValue'));
-			});
-
-		}); */
 		//Initialize variables
 		var monthSal=0;
 		var currentAge=37;
@@ -46,83 +36,34 @@
 
 		$(".btn-cal").click(function(){
 			
-			//Reset input values first
-			//$('.current-age').val("");
-			//$('.pension-age').val("");
-			//$('.month-sal').val("");
-			//console.log("CHANDRA: "+$("#slider3").slider('getValue'));
-			
-			
-			
 			currentAge=parseFloat($("#slider1").slider('getValue'));
 			pensionAge=parseFloat($("#slider2").slider('getValue'));
 			monthSal=parseFloat($('.month-sal').val());
 			annuityYrs=parseInt(pensionAge-currentAge);
-			console.log("current Age: "+currentAge);
-			console.log("current age monthly Salary: "+monthSal);
-			console.log("pension  Age: "+pensionAge);
-			console.log("Calculate Annuity for next: "+annuityYrs+" Years");
 			for (i = 0; i <=annuityYrs; i++) {
 				
 				if(i==0){
 					console.log(i+" current age monthly Salary: "+monthSal);
 					prevYrMonthlySal=monthSal;
-					console.log(i+" current age employee Contribution:"+empContri+'%= '+(monthSal*empContri/100));
-					console.log(i+" current age employer Contribution:"+emplyrContri+'%= '+(monthSal*emplyrContri/100));
-					console.log(i+" current age state contribution:"+stateContri+'%= '+(monthSal*stateContri/100));
-					
-					//currentTotalContri= parseFloat((monthSal*empContri/100)+(monthSal*emplyrContri/100)+(monthSal*stateContri/100));
 					currentTotalContri= parseFloat((monthSal*empContri/100)+(monthSal*emplyrContri/100)+(monthSal*stateContri/100));
-					
-					console.log(i+" current age Year Total Contribution: "+currentTotalContri);
-					console.log(i+" previous Total Cummulative Contribution: "+prevTotalCumContri);
-					currentTotalCumContri=prevTotalCumContri+currentTotalContri;
-					console.log(i+" current age Year TotalCumulative Contribution: "+currentTotalCumContri);
 				}else{
-					
 
-					console.log(i+" previous Total Contribution : "+currentTotalContri);
 					prevTotalCumContri=currentTotalCumContri;
 					prevYrMonthlySal=monthSal;
-					console.log(i+" previous Total Cum Contribution : "+currentTotalCumContri);
-					console.log(i+" previous year monthly salary: "+prevYrMonthlySal);
-					console.log(i+" next year hike: "+salHike+'%= '+monthSal*(salHike/100));
-					
 					monthSal=prevYrMonthlySal+parseFloat(monthSal*(salHike/100));
-					
-					console.log(i+" next year monthly salary: "+monthSal);
-					//monthSal+=parseFloat(monthSal)*(salHike/100);
-					console.log(i+" next year employee Contribution:"+empContri+'%= '+(monthSal*empContri/100));
-					console.log(i+" next year employer Contribution:"+emplyrContri+'%= '+(monthSal*emplyrContri/100));
-					console.log(i+" next year state contribution:"+stateContri+'%= '+(monthSal*stateContri/100));
-					currentTotalContri= parseFloat((monthSal*empContri/100)+(monthSal*emplyrContri/100)+(monthSal*stateContri/100));
-					
-					console.log(i+" Prev Total Cumm Contribution: "+prevTotalCumContri);
-					console.log(i+" next Year Total Contribution: "+currentTotalContri);
-					console.log(i+" that Year TotalCumulative Contribution: "+(prevTotalCumContri+currentTotalContri));					
+					currentTotalContri= parseFloat((monthSal*empContri/100)+(monthSal*emplyrContri/100)+(monthSal*stateContri/100));				
 					currentTotalCumContri=prevTotalCumContri+currentTotalContri;
-				
-					console.log(i+" next year Total cumulative Contribution: (current+ previous)  "+currentTotalCumContri);
-					console.log("*****************************");
 				}
 			prevTotalCumContri=currentTotalContri;
 			prevYrMonthlySal=monthSal;
 		}
-		console.log("Pension Age calculations as below:");
-		console.log("pension age monthly Salary: "+monthSal+" pension age Total Contribution: "+currentTotalContri+" pension age Total Cumulative Contribution: "+currentTotalCumContri);
-		console.log("*****************************");
-		
+		currentTotalCumContri);
 		Factor=currentTotalCumContri/100000;
 		monthlyAnnuity=Math.round(Factor*annuityConstant);
-		console.log("monthlyAnnuity::::: "+monthlyAnnuity)
 		$("#slider3").slider('setValue', monthlyAnnuity);
 		annualAnnuity=Math.round(monthlyAnnuity*12);
 		$(".pension-pot").text(monthlyAnnuity);
 		//$(".pension-pot-yearly").text(annualAnnuity);
-		
-		console.log("Factor: "+Factor);
-		console.log("monthlyAnnuity: "+monthlyAnnuity);
-		console.log("annualAnnuity: "+annualAnnuity);
 		});
 		//Reset Form
 		$(".btn-reset").click(function(){
@@ -142,18 +83,8 @@
 			 
 			 $("#slider1").slider('refresh');
 			 $("#slider2").slider('refresh');
-			 $("#slider3").slider('refresh');
-			 //
-			 
-/* 			$('.current-age').val("");
-			$('.pension-age').val("");
-			$('.month-sal').val("");
-			$(".pension-pot").text("");
-			$(".pension-pot-yearly").text(""); */
-		
+			 $("#slider3").slider('refresh');		
 		});
-
-
 }());
 	
 	
